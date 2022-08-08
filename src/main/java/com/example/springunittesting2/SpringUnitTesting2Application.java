7b977785286b8ef3766338a6d862355f8656fb89
seanjungmsba <seanjeong1996@gmail.com>
2022-08-08 12:27:24 -0400

    Initial setup

---
+++ src/main/java/com/example/springunittesting2/SpringUnitTesting2Application.java
@@ -0,0 +1,13 @@
+package com.example.springunittesting2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUnitTesting2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUnitTesting2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springunittesting2/SpringUnitTesting2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springunittesting2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUnitTesting2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
